804c85b4e6f24d463274c51999f17e8de0e6b5a4
Wachenng <wcl2036@outlook.com>
2025-04-01 18:22:07 +0800

    IO协程调度模块04.

--- sylar/scheduler.h
+++ sylar/scheduler.h
@@ -84,6 +84,8 @@ protected:
     virtual void idle();
 
     void setThis();
+
+    bool hasIdleThreads() { return m_idleThreadCount > 0;}
 private:
     template<class FiberOrCb>
     bool scheduleNoLock(FiberOrCb fc, int thread){
