1da4417ba00898f8bead5e633a3a4cf8287fd0a8
Wachenng <wcl2036@outlook.com>
2025-04-01 17:20:55 +0800

    IO协程调度模块01.

---
+++ sylar/iomanager.cc
@@ -0,0 +1,146 @@
+#include "iomanager.h"
+#include "macro.h"
+#include "log.h"
+
+#include <sys/epoll.h>
+#include <unistd.h>
+#include <fcntl.h> 
+#include <errno.h>
+#include <string.h>
+
+namespace sylar {
+
+static sylar::Logger::ptr g_logger = SYLAR_LOG_NAME("system");
+
+IOManager::IOManager(size_t threads, bool use_caller, const std::string& name)
+    :Scheduler(threads, use_caller, name) {
+    m_epfd = epoll_create(5000);
+    SYLAR_ASSERT(m_epfd > 0);
+    
+    int rt = pipe(m_tickleFds);
+    SYLAR_ASSERT(rt);
+
+    epoll_event event;
+    memset(&event, 0, sizeof(epoll_event));
+    //水平触发 | 边沿触发--只触发一次
+    event.events = EPOLLIN | EPOLLET;
+    event.data.fd = m_tickleFds[0];
+
+    rt = fcntl(m_tickleFds[0], F_SETFL, O_NONBLOCK);
+    SYLAR_ASSERT(rt);
+
+    rt = epoll_ctl(m_epfd, EPOLL_CTL_ADD, m_tickleFds[0], &event);
+    SYLAR_ASSERT(rt);
+
+    contextResize(32);
+
+    start();
+
+}
+IOManager::~IOManager() {
+    stop();
+    close(m_epfd);
+    close(m_tickleFds[0]);
+    close(m_tickleFds[1]);
+
+    for(size_t i = 0; i < m_fdContexts.size(); ++i) {
+        if(m_fdContexts[i]) {
+            delete m_fdContexts[i];
+        }
+    }
+}
+
+void IOManager::contextResize(size_t size) {
+    m_fdContexts.resize(size);
+
+    for(size_t i = 0; i < m_fdContexts.size(); ++i) {
+        if(!m_fdContexts[i]) {
+            m_fdContexts[i] = new FdContext;
+            m_fdContexts[i]->fd = i;
+        }
+    }
+}
+
+
+int IOManager::addEvent(int fd, Event event, std::function<void()> cb) {
+    FdContext* fd_ctx = nullptr;
+    RWMutexType::ReadLock lock(m_mutex);
+
+    if((int)m_fdContexts.size() > fd) {
+        fd_ctx = m_fdContexts[fd];
+        lock.unlock();
+    } else {
+        lock.unlock();
+        RWMutexType::WriteLock lock2(m_mutex);
+        contextResize(m_fdContexts.size() * 1.5);
+        fd_ctx = m_fdContexts[fd];
+    }
+
+    FdContext::MutexType::Lock lock2(fd_ctx->mutex);
+    if(fd_ctx->events & event) {
+        SYLAR_LOG_ERROR(g_logger) << "addEvent assert fd=" << fd
+                                  << " event=" << event
+                                  << " fd_ctx.events=" << fd_ctx->events;
+        SYLAR_ASSERT(!(fd_ctx->events & event));
+    }
+
+    int op = fd_ctx->events ? EPOLL_CTL_MOD : EPOLL_CTL_ADD;
+    epoll_event epevent;
+    epevent.events = EPOLLET | fd_ctx->events | event;
+    epevent.data.ptr = fd_ctx;
+
+    int rt = epoll_ctl(m_epfd, op, fd, &epevent);
+    if(rt) {
+        SYLAR_LOG_ERROR(g_logger) << "epoll_ctl(" << m_epfd << ", "
+                                  << op << ", " << fd << ", " << epevent.events << "):"
+                                  << rt << " (" << errno << ") (" << strerror(errno) << ")";
+        return -1;
+    }
+
+    ++m_pendingEventCount;
+    fd_ctx->events = (Event)(fd_ctx->events | event);
+    FdContext::EventContext& event_ctx = fd_ctx->getContext(event);
+    SYLAR_ASSERT(!event_ctx.scheduler
+                 && !event_ctx.fiber
+                 && !event_ctx.cb);
+                
+    event_ctx.scheduler = Scheduler::GetThis();
+    if(cb) {
+        event_ctx.cb.swap(cb);
+    } else {
+        event_ctx.fiber = Fiber::GetThis();
+        SYLAR_ASSERT(event_ctx.fiber->getState() == Fiber::EXEC);
+    }
+    return 0;
+}
+
+bool IOManager::delEvent(int fd, Event event) {
+
+}
+
+bool IOManager::cancelEvent(int fd, Event event) {
+
+}
+
+bool IOManager::cancelAll(int fd) {
+
+}
+
+IOManager * IOManager::GetThis() {
+
+}
+
+void IOManager::tickle() {
+
+}
+
+bool IOManager::stopping() {
+
+}
+
+void IOManager::idle() {
+
+}
+
+
+}
---
+++ sylar/iomanager.h
@@ -0,0 +1,104 @@
+#ifndef __SYLAR_IO_MANAGER_H__
+#define __SYLAR_IO_MANAGER_H__
+
+#include "scheduler.h"
+
+
+/*
+  IOManager(epoll) --> Scheduler
+    |
+    |
+    v
+  idle(epoll_wait)
+
+     信号量
+  putMessage(msg,) + 信号量1
+  message_queue
+        |
+        |--------Thread
+        |--------Thread
+            wait()-信号量1 RecvMessage(msg,1)
+    PS：如果信号量没有，那么就会一直阻塞在这里，存在问题是如果信号量阻塞，那么整个线程都被阻塞了
+
+
+异步IO, 等待数据返回。 epoll_wait()如果没有数据会阻塞在epoll_wait()上，直到有数据返回。或有人向里边放任务
+*/
+namespace sylar {
+
+class IOManager : public Scheduler{
+public:
+    typedef std::shared_ptr<IOManager> ptr;
+    typedef RWMutex RWMutexType;
+
+    enum Event {
+        NONE    = 0x0,
+        READ    = 0x1,
+        WRITE   = 0x4
+    };
+
+private:
+    struct FdContext {
+        typedef Mutex MutexType;
+        struct EventContext {
+            Scheduler* scheduler = nullptr;         //表示在哪一个调度器上执行
+            Fiber::ptr fiber;                       //表示要执行的fiber
+            std::function<void()> cb;               //表示要执行的函数
+        };
+
+        EventContext& getContext(Event event);
+
+        int fd = 0;                  //事件描述符
+        EventContext read;       //读事件
+        EventContext write;      //写事件
+        Event events = NONE;   //已经注册的事件
+        MutexType mutex;
+    };
+
+public:
+    IOManager(size_t threads = 1, bool use_caller = true, const std::string& name = "");
+    ~IOManager();
+
+    /**
+     * @brief 增加事件
+     * @param
+     * @return 0 success -1 error
+     */
+    int addEvent(int fd, Event event, std::function<void()> cb = nullptr);
+
+    /**
+     * @brief 删除事件--直接删除了
+     */
+    bool delEvent(int fd, Event event);
+
+    /**
+     * @brief 取消事件--如果存在触发条件，那么先触发事件再删除事件
+     */
+    bool cancelEvent(int fd, Event event);
+
+    bool cancelAll(int fd);
+
+    /**
+     * @brief 获取当前IOManager
+     */
+    static IOManager * GetThis();
+
+protected:
+    void tickle() override;
+    bool stopping() override;
+    void idle() override;
+
+    void contextResize(size_t size);
+private:
+    int m_epfd = 0;
+    //通过管道来唤醒，不通过异步IO来唤醒，即进程间通讯方式
+    //不通过信号量+1-1的方式，而是通过向里边写数据的方式
+    int m_tickleFds[2];
+    //现在等待执行的事件数量
+    std::atomic<size_t> m_pendingEventCount = {0};
+    RWMutexType m_mutex;
+    std::vector<FdContext*> m_fdContexts;
+};
+
+}
+
+#endif
